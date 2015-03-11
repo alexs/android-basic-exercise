ba8f8d65226b5f780796dd04d7ea55f79eb08f2b
Alex S <alexs1010@gmail.com>
2015-03-11 14:16:20 -0600

    Add logic for MainActivity

--- app/src/main/java/com/github/alexs/android_basic_exercise/MainActivity.java
+++ app/src/main/java/com/github/alexs/android_basic_exercise/MainActivity.java
@@ -4,14 +4,72 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    double EURO2USD = 1.399;
+    double PESO2USD = 0.07434;
+
+    Button btnConvertir;
+    Button btnLimpiar;
+    EditText txtDolares;
+    EditText txtEuros;
+    EditText txtPesos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtDolares = (EditText)findViewById(R.id.txtUSDollars);
+        txtDolares.setHint("Cantidad en dolares");
+        txtEuros = (EditText)findViewById(R.id.editText1);
+        txtEuros.setHint("Cantidad en Euros");
+        txtPesos = (EditText)findViewById(R.id.editText2);
+        txtPesos.setHint("Cantidad en Pesos MX");
+
+        btnLimpiar = (Button) findViewById(R.id.btnLimpiar);
+        btnLimpiar.setOnClickListener(new OnClickListener(){
+            // @Override
+            public void onClick(View v){
+                txtDolares.setText("");
+                txtEuros.setText("");
+                txtPesos.setText("");
+            }
+        });
+
+        btnConvertir = (Button) findViewById(R.id.btnConvertir);
+        btnConvertir.setOnClickListener(new OnClickListener(){
+            public void onClick(View v){
+                String usdStr = txtDolares.getText().toString();
+                if (usdStr.matches("")) {
+                    Toast.makeText(v.getContext(),
+                            "Debes ingresar dólares, esta app solo convierte de dolares a las demás divisas",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else{
+                    try{
+
+                        double usd = Double.parseDouble(usdStr);
+
+                        String euros = String.format("%.2f",(usd/EURO2USD));
+                        String pesos = String.format("%.2f",(usd/PESO2USD));
+
+                        txtEuros.setText(euros);
+                        txtPesos.setText(pesos);
+                    }
+                    catch(Exception e){
+                        Toast.makeText(v.getContext(), e.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
     }
 
 
